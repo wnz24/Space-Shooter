8c6e927ddd5656b8557530551f8e2069f4ee7400
Wajeeha Noor <112068947+wnz24@users.noreply.github.com>
2025-10-16 16:30:39 +0500

    Add critters

---
+++ Assets/Scripts/Critter1.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class Critter1 : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite[] sprites;
+
+    private float moveSPeed;
+    private Vector3 targetPosition;
+    private Quaternion targetRotation;  
+
+    private float moveTimer;
+    private float moveInterval;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
+        moveSPeed = Random.Range(0.5f, 3f);
+        GenerateRandomPositions();
+        moveInterval = Random.Range(0.5f, 2f);
+        moveTimer = moveInterval;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveTimer > 0) { 
+            moveTimer -= Time.deltaTime;
+        } else {
+            GenerateRandomPositions();
+            moveInterval = Random.Range(0.1f, 2f);
+            moveTimer = moveInterval;
+        }
+        transform.position = Vector3.MoveTowards(transform.position,targetPosition,moveSPeed * Time.deltaTime);
+
+        Vector3 relativePOS = targetPosition - transform.position;
+        if(relativePOS != Vector3.zero){
+            targetRotation = Quaternion.LookRotation(Vector3.forward, relativePOS);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 1000 * Time.deltaTime);
+
+
+        }
+
+        float moveX = (GameManager.Instance.worldSpeed * PlayerController.Instance.boost) * Time.deltaTime;
+        transform.position += new Vector3(moveX, 0);
+    }
+
+    private void GenerateRandomPositions()
+    {
+        float RandomX = Random.Range(-5f, 5f);
+        float RandomY = Random.Range(-5f, 5f);
+        targetPosition = new Vector2(RandomX, RandomY);
+    }
+}
